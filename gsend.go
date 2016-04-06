66fa7515cd67c797335ed0aa849daa221d3b9608
Carl Johnson <cmjohnson@pbs.org>
2016-04-06 10:21:10 -0400

    Refactor: Make string interpolation easier to read

--- gsend.go
+++ gsend.go
@@ -33,8 +33,7 @@ func main() {
 		return
 	}
 
-	body := "To: " + *to + "\r\nSubject: " +
-		*subject + "\r\n\r\n" + *msg
+	body := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", *to, *from, *msg)
 	auth := smtp.PlainAuth("", *from, *pwd, "smtp.gmail.com")
 	err := smtp.SendMail("smtp.gmail.com:587", auth, *from,
 		[]string{*to}, []byte(body))
